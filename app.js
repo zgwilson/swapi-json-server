16fb4c13849db82f40b8063654c3ca1a0aa662d3
johnlindquist <johnlindquist@gmail.com>
2018-03-19 09:04:10 -0600

    adding caching to images

--- app.js
+++ app.js
@@ -1,124 +1,178 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server")
 
-const films = require('./films.json');
-const people = require('./people.json');
-const planets = require('./planets.json');
-const species = require('./species.json');
-const starships = require('./starships.json');
-const transport = require('./transport.json');
-const vehicles = require('./vehicles.json');
+const films = require("./films.json")
+const people = require("./people.json")
+const planets = require("./planets.json")
+const species = require("./species.json")
+const starships = require("./starships.json")
+const transport = require("./transport.json")
+const vehicles = require("./vehicles.json")
 
 const db = {
-    films,
-    people,
-    planets,
-    species,
-    starships,
-    transport,
-    vehicles
-};
-
+  films,
+  people,
+  planets,
+  species,
+  starships,
+  transport,
+  vehicles
+}
 
 /**
  * Lifting the "fields" to the root of the object
  * Using the "pk" as the "id"
  */
-const fdb = Object.keys(db).reduce((acc, current) => {
-    acc[current] = db[current].map((item) => {
-        return Object.assign(
-            item.fields,
-            { id: item.pk }
-        );
-    });
+const fdb = Object.keys(db).reduce(
+  (acc, current) => {
+    acc[current] = db[current].map(item => {
+      return Object.assign(item.fields, {
+        id: item.pk
+      })
+    })
 
-    return acc;
-}, {});
+    return acc
+  },
+  {}
+)
 
 /**
  * Vehicles and Starships "extend" Transport...
  */
 
-const xdb = Object.keys(fdb).reduce((acc, current)=>{
-    if(current === "starships" || current === "vehicles"){
-        acc[current] = acc[current].map((item)=>{
-            const transport = acc["transport"].find((elm)=>{                    
-                return elm.id === item.id;
-            });
-                        
-            if(transport){                
-                return Object.assign(item,transport)
-            }
-            
-            return item;            
-        })
-    }        
-    return acc;
-}, fdb);
+const xdb = Object.keys(fdb).reduce(
+  (acc, current) => {
+    if (
+      current === "starships" ||
+      current === "vehicles"
+    ) {
+      acc[current] = acc[current].map(item => {
+        const transport = acc["transport"].find(
+          elm => {
+            return elm.id === item.id
+          }
+        )
+
+        if (transport) {
+          return Object.assign(item, transport)
+        }
+
+        return item
+      })
+    }
+    return acc
+  },
+  fdb
+)
 
 /**
  * Relation definitions to match them with swapi.co
  */
 const relations = [
-    {   
-        name: "people",
-        relation:[
-            {alias: "vehicles", table: "vehicles", name: "pilots", type: Array},
-            {alias: "starships", table: "starships", name: "pilots", type: Array},
-            {alias: "films", table: "films", name: "characters", type: Array},
-        ]
-    },
-    {   
+  {
+    name: "people",
+    relation: [
+      {
+        alias: "vehicles",
+        table: "vehicles",
+        name: "pilots",
+        type: Array
+      },
+      {
+        alias: "starships",
+        table: "starships",
+        name: "pilots",
+        type: Array
+      },
+      {
+        alias: "films",
+        table: "films",
+        name: "characters",
+        type: Array
+      }
+    ]
+  },
+  {
+    name: "planets",
+    relation: [
+      {
+        alias: "residents",
+        table: "people",
+        name: "homeworld",
+        type: Number
+      },
+      {
+        alias: "films",
+        table: "films",
         name: "planets",
-        relation:[
-            {alias: "residents", table: "people", name: "homeworld", type: Number},
-            {alias: "films", table: "films", name: "planets", type: Array}
-        ]
-    },
-    {   
+        type: Array
+      }
+    ]
+  },
+  {
+    name: "starships",
+    relation: [
+      {
+        alias: "films",
+        table: "films",
         name: "starships",
-        relation:[
-            {alias: "films", table: "films", name: "starships", type: Array}
-        ]
-    }
-];
+        type: Array
+      }
+    ]
+  }
+]
 
 function addRelation(db, host, relations) {
-    return relations.reduce((acc, relation) => {
-        const alias = relation.alias;
-        const table = relation.table;
-        const name = relation.name;
-        if(relation.type === Number){
-            acc[alias] = db[table]
-                .filter(item => item[name] === host.id)
-                .map(item => item.id);
-                
-            return acc;                
-        }
-                
-        acc[alias] = db[table]
-            .filter(item => item[name].indexOf(host.id) > -1)
-            .map(item => item.id);
+  return relations.reduce((acc, relation) => {
+    const alias = relation.alias
+    const table = relation.table
+    const name = relation.name
+    if (relation.type === Number) {
+      acc[alias] = db[table]
+        .filter(item => item[name] === host.id)
+        .map(item => item.id)
 
-        return acc;
-    }, {})
+      return acc
+    }
+
+    acc[alias] = db[table]
+      .filter(
+        item => item[name].indexOf(host.id) > -1
+      )
+      .map(item => item.id)
+
+    return acc
+  }, {})
 }
 
-const relationDb = relations.reduce((acc, current)=>{
-    acc[current.name] = acc[current.name].map((item) => {
+const relationDb = relations.reduce(
+  (acc, current) => {
+    acc[current.name] = acc[current.name].map(
+      item => {
         return Object.assign(
-            item,
-            addRelation(acc, item, current.relation)
+          item,
+          addRelation(acc, item, current.relation)
         )
-    });
-    return acc;
-}, xdb);
+      }
+    )
+    return acc
+  },
+  xdb
+)
 
+const server = jsonServer.create()
 
-const server = jsonServer.create();
-server.use(jsonServer.defaults());
+const serveStatic = require("serve-static")
+const path = require("path")
+server.use(
+  serveStatic(path.join(__dirname, "public"), {
+    maxAge: "1d"
+  })
+)
 
-const router = jsonServer.router(relationDb);
-server.use(router);
+server.use(jsonServer.defaults())
 
-server.listen(3000);
+const router = jsonServer.router(relationDb)
+server.use(router)
+
+server.listen(3000)
 console.log(`Server started on port 3000`)
